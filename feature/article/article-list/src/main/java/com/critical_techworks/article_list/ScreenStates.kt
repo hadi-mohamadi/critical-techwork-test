7c46bb52a88671e9a597b6d6a1518e264f1aeaa7
hadi.mohammadi <hadi.mohammadi@tapsi.cab>
2022-05-13 21:04:03 +0430

    add list ui

---
+++ feature/article/article-list/src/main/java/com/critical_techworks/article_list/ScreenStates.kt
@@ -0,0 +1,81 @@
+package com.critical_techworks.article_list
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.critical_techworks.core_ui.theme.LabelRetry
+
+@Composable
+fun LoadingView(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CircularProgressIndicator()
+    }
+}
+
+@Preview
+@Composable
+private fun LoadingViewPreview(
+    modifier: Modifier = Modifier
+) {
+    LoadingView()
+}
+
+@Composable
+fun LoadingItem() {
+    CircularProgressIndicator(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+            .wrapContentWidth(Alignment.CenterHorizontally)
+    )
+}
+
+@Preview
+@Composable
+private fun LoadingItemPreview() {
+    LoadingItem()
+}
+
+@Composable
+fun ErrorItem(
+    message: String,
+    modifier: Modifier = Modifier,
+    onClickRetry: () -> Unit
+) {
+    Row(
+        modifier = modifier.padding(16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = message,
+            maxLines = 1,
+            modifier = Modifier.weight(1f),
+            style = MaterialTheme.typography.h6,
+            color = Color.Red
+        )
+        OutlinedButton(onClick = onClickRetry) {
+            Text(text = LabelRetry)
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun ErrorItemPreview() {
+    ErrorItem(message = "connection error", onClickRetry = {})
+}
